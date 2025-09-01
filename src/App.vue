d5e0dd93bba3cf58010cc81b2f3567be8da3bca3
belkud <vlad-belkud@yandex.ru>
2025-09-01 17:34:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import AddFile from './components/AddFile.vue'
+
+</script>
+
+
+<template>
+  <AddFile msg="Hello from add file" />
+  <HelloWorld msg="Vite + Vue5555555555555555" />
+  <div class="color">
+    <a  href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    hjkhiuhgiu
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #050a77aa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #f5bd05aa);
+}
+.color {
+ color: red;
+}
+</style>
---
+++ src/components/AddFile.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+let str = ref('our_page')
+let bool =ref(false)
+function click2() {
+    console.log('Вызов функции');
+    
+}
+ 
+</script>
+
+
+
+<template>
+    <div class="myColor" v-if="bool"   @click="str+='!'">{{ str }}</div>
+    <div class="myColor" v-else  @click="str+='!'">{{ str.split('').reverse().join('') }}</div>
+    <button v-on:click="bool=!bool">reverse</button>
+  <h1>{{ msg }}</h1>
+        <button @click="count++">счёт: {{ count }} </button>
+ 
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter ===========
+  </p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a
+      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      target="_blank"
+      >Vue Docs Scaling up Guide</a
+    >.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+<button @click="click2">My button {{ 12+10 }}</button>
+<input @keydown.prevent v-on:keydown="console.log('test')" >My button</input>
+
+
+</template>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+
+.myColor {
+    color: orange
+}
+
+
+
+
+</style>
