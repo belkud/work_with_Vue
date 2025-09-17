839ac91a3eec156e8a5a233023021e81f0463d4f
belkud <vlad-belkud@yandex.ru>
2025-09-17 17:19:17 +0300

    make table td th hover on vue

--- src/components/AddFile4.vue
+++ src/components/AddFile4.vue
@@ -6,6 +6,8 @@ let inputText = ref('')
 let width = ref(150)
 // let divWidth  = ref(150)
 
+const newColor = 'orange'
+
 console.log('12345');
 
 function changeWidth() {
@@ -13,20 +15,62 @@ function changeWidth() {
     console.log(width.value);
     // divWidth.style.width= 150 + width.value + 'px'
     // console.log(width);
-    
 }
+    
 
 
 
-let texxt = ref('')
 
 function showEl (e:any) {
         console.log(e.target);
         
 }
+
+function showTarget(e:any) {
+    e.target.style.background = 'red'
+}
+
+function showTarget2(e:any) {
+    e.target.style.background = ''
+}
+
 </script>
 
 <template>
+<h2>AddFile4</h2>
+
+
+
+
+<h3>Работа с таблицей</h3>
+
+<table @mousemove="showTarget"  @mouseout="showTarget2">
+    <tr>
+        <th>header1</th>
+        <th>header2</th>
+        <th>header3</th>
+    </tr>
+    <tr>
+        <td>cell1</td>
+        <td>cell2</td>
+        <td>cell3</td>
+    </tr>
+    <tr>
+        <td>cell1</td>
+        <td>cell2</td>
+        <td>cell3</td>
+    </tr>
+    <tr>
+        <td>cell1</td>
+        <td>cell2</td>
+        <td>cell3</td>
+    </tr>
+</table>
+
+
+<div :style="{color: newColor, background:'red', width: '150px'}"> добавление стилей</div>
+
+
 
 <input type="checkbox" @click="showEl" checked id="checkbox" />
 
@@ -53,13 +97,8 @@ function showEl (e:any) {
     </form>
 
 
-<input v-model="texxt" type="text">
-<div>{{ texxt }}</div>
 
 
-
-
-    <h2>AddFile4</h2>
 
     <slot></slot>
   
@@ -80,6 +119,20 @@ function showEl (e:any) {
         min-width: 150px;
         background-color: orange
     }
+
+    table {
+        border-collapse:collapse;
+        border: 1px solid gray;
+        background: lightblue;
+    }
+    table td, table th{
+        padding: 5px;
+        border: 1px solid gray;
+    }
+
+    /* table td:hover {
+        background: orange;
+    } */
 </style>
 
 
