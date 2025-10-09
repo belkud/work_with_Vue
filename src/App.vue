18d0dffea6065b344eca465ba0052e6c591065fe
belkud <vlad-belkud@yandex.ru>
2025-10-09 12:07:24 +0300

    update readme

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import AddFile from './components/AddFile.vue'
 import AddFile4 from "./components/AddFile4.vue";
 import RandomNumber from "./components/RandomNumber.vue";
 import ShowCoordinate from "./components/ShowCoordinate.vue";
-import ButtonShowText  from "./components/ButtonShowText.vue";
-import MoveProgressBar  from "./components/MoveProgressBar.vue";
+import ButtonShowText from "./components/ButtonShowText.vue";
+import MoveProgressBar from "./components/MoveProgressBar.vue";
 import NewsList from "./components/NewsList.vue";
 
 import TheButton from "./components/TheButton.vue";
@@ -46,80 +46,84 @@ import HomeWork from "./components/HomeWork.vue";
 
 <template>
 
-<home-work/>
+  <home-work />
 
 
+  <!--! <RouterLink/> -  компонент, предоставляемый библиотекой Vue Router для навигации в одностраничных приложениях (SPA). В отличие от обычной HTML-ссылки (<a>), он обеспечивает переход между страницами без перезагрузки -->
   <div style="display: flex; margin: 10px; gap: 25px;">
     <RouterLink to="/">На главную</RouterLink>
     <RouterLink to="/about">About</RouterLink>
   </div>
 
-<RouterView></RouterView>
+  <!--! <RouterView> — это специальный компонент, предоставляемый Vue Router, который выступает в роли "контейнера" для отображения компонентов, соответствующих текущему URL. Он динамически подставляет нужный компонент в зависимости от маршрута -->
+  <RouterView></RouterView>
+
+
+ <!--! очень крутая комбинация как 'alt' + 'Z' для форатирования текста в докукете -->
+
+<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iure eaque assumenda quisquam quasi dolor similique excepturi quod repellat enim maxime quia explicabo cum sunt fuga dolorum, recusandae expedita sequi.</div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
 
 
 
 
 
 
+  <users-forum></users-forum>
 
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+  <testvue></testvue>
 
 
 
 
 
 
-<users-forum></users-forum>
+  <AddFile4>
+    <div>ssssssss</div>
+  </AddFile4>
 
-<testvue></testvue>
-
-
-
-
-
-
-<AddFile4>
-  <div>ssssssss</div>
-</AddFile4>
-
-<news-list/>
-<br><hr><br >
+  <news-list />
+  <br>
+  <hr><br>
 
 
 
-  <add-file msg="Hello from add file">      <!--! вторая аналогичная запись -->
-    <p>Текст через тег slot  в дочернем элементе</p> <!--! с помощью тега slot в tempalte файла AddFile -->
+  <add-file msg="Hello from add file"> <!--! вторая аналогичная запись -->
+    <p>Текст через тег slot в дочернем элементе</p> <!--! с помощью тега slot в tempalte файла AddFile -->
   </add-file>
 
 
-  
-
-
-<random-number/>
-<br><hr><br>
-
-<show-coordinate/>
-<br><hr><br>
-
-
-<button-show-text/>
-<br><hr><br>
-
-<move-progress-bar/>
-<br><hr><br >
 
 
 
-<TheButton test-props="proooooops">
-  <div>ppppppp</div>
-</TheButton>
+  <random-number />
+  <br>
+  <hr><br>
+
+  <show-coordinate />
+  <br>
+  <hr><br>
+
+
+  <button-show-text />
+  <br>
+  <hr><br>
+
+  <move-progress-bar />
+  <br>
+  <hr><br>
+
+
+
+  <TheButton test-props="proooooops">
+    <div>ppppppp</div>
+  </TheButton>
 
 
 
@@ -129,23 +133,23 @@ import HomeWork from "./components/HomeWork.vue";
 
 
 
-    <!-- <add-file msg="Hello from add file"/>  <!--  ! первая запись со слэшом -->
+  <!-- <add-file msg="Hello from add file"/>  <!--  ! первая запись со слэшом -->
 
-    
+
 
   <!--! <AddFile2 msg2="234234234"></AddFile2>  ! вторая аналогичная запись -->
 
 
 
-  
-  
-  
+
+
+
 
   <div class="color">
-    <a  href="https://vite.dev" target="_blank">
+    <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-     
+
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
@@ -153,21 +157,23 @@ import HomeWork from "./components/HomeWork.vue";
 </template>
 
 <style scoped>
-
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #050a77aa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #f5bd05aa);
 }
+
 .color {
- color: green;
+  color: green;
 }
 </style>
 
@@ -185,4 +191,3 @@ import HomeWork from "./components/HomeWork.vue";
 <!--! git config --global user.name "Your Name" -->
 <!--! git config --global user.email "your@email.com"-->
 <!--! git log --graph - краткая история коммитов-->
-
