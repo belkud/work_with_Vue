c7b1633762004a35f8d027df8da87db89bb71764
belkud <vlad-belkud@yandex.ru>
2025-12-29 23:37:12 +0300

    create App2.vue

---
+++ src/App2.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <h2 style="color: red;">План по изучению vue JS</h2>
+    <div>
+        <div >Информация с первой кнопки</div>
+        <div >Информация со второй кнопки</div>
+        <div >Информация с третьей кнопки</div>
+        <div >Информация с четвертой кнопки</div>
+        <div >Информация с пятой кнопки</div>
+    </div>
+    <div class="buttonLocation">
+        <button @click="">Основы</button>
+        <button>Компоненты</button>
+        <button>Роутер</button>
+        <button>Vuex</button>
+        <button>Composition</button>
+    </div>
+    <div class="buttonLocation">
+        <button>Назад</button>
+        <button>Вперед</button>
+    </div>
+</template>
+
+
+<style scoped>
+.buttonLocation {
+    gap: 10px;
+    display: flex;
+    margin: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,23 @@
 import { createApp } from 'vue' 
 
 import './style.css'
-// import './style.css'
 
 //! 2. Импортируем корневой компонент нашего приложения
 import App from './App.vue'
-import TheButton from "./components/TheButton.vue";
+import App2 from './App2.vue'
+// import TheButton from "./components/TheButton.vue";
 // import {router} from './router/index.ts'
 import {router} from './router';
 
 //! 3. - 4. Создаем экземпляр приложения, передав ему корневой компонент
 //! "Монтируем" приложение в DOM - указываем, куда именно его встроить
 const app = createApp(App) // начальная страница при загрузке
-app.component('TheButton', TheButton.vue)
+const app2 = createApp(App2) // начальная страница при загрузке
+// app.component('TheButton', TheButton.vue)
 app.use(router)
+
 app.mount('#app')
+app2.mount('#app2')
 
 
 
